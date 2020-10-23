96e934f40e7d44c09fb9c4634b8e7811270e6753
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 13:03:04 -0300

    google-maps-services-java,com.google.maps.internal,RateLimitExecutorService,isShutdown#,MoveMethod,OK,false

--- src/main/java/com/google/maps/internal/RateLimitExecutorService.java
+++ src/main/java/com/google/maps/internal/RateLimitExecutorService.java
@@ -42,7 +42,7 @@ public class RateLimitExecutorService implements ExecutorService, Runnable {
   // It's important we set Ok's second arg to threadFactory(.., true) to ensure the threads are
   // killed when the app exits. For synchronous requests this is ideal but it means any async
   // requests still pending after termination will be killed.
-  private final ExecutorService delegate =
+  public final ExecutorService delegate =
       new ThreadPoolExecutor(
           Runtime.getRuntime().availableProcessors(),
           Integer.MAX_VALUE,
@@ -132,8 +132,8 @@ public class RateLimitExecutorService implements ExecutorService, Runnable {
 
   @Override
   public boolean isShutdown() {
-    return delegate.isShutdown();
-  }
+	return rateLimiter.isShutdown(this);
+}
 
   @Override
   public boolean isTerminated() {
