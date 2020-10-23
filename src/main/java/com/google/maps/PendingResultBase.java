6221c38a23967d3f5f4a321b45e1ca3c3aab9230
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 20:42:03 -0300

    google-maps-services-java,com.google.maps,DirectionsApiRequest,destination#QLatLng,PullUpMethod,BUILD FAILED,false

--- src/main/java/com/google/maps/PendingResultBase.java
+++ src/main/java/com/google/maps/PendingResultBase.java
@@ -19,6 +19,8 @@ import com.google.maps.errors.ApiException;
 import com.google.maps.internal.ApiConfig;
 import com.google.maps.internal.ApiResponse;
 import com.google.maps.internal.StringJoin.UrlValue;
+import com.google.maps.model.LatLng;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -168,5 +170,15 @@ abstract class PendingResultBase<T, A extends PendingResultBase<T, A, R>, R exte
    */
   public A custom(String parameter, String value) {
     return param(parameter, value);
+  }
+
+/**
+   * The destination, as a latitude/longitude location.
+   *
+   * @param destination The ending location for the Directions request.
+   * @return Returns this {@code DirectionsApiRequest} for call chaining.
+   */
+public DirectionsApiRequest destination(LatLng destination) {
+    return destination(destination.toString());
   }
 }
