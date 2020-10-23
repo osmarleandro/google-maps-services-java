b6acb02b171f81f2dc3aa241b96f3e0db6f66cf1
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 13:08:22 -0300

    google-maps-services-java,com.google.maps.internal.ratelimiter,RateLimiter,tryAcquire#I,MoveMethod,OK,false

--- src/main/java/com/google/maps/internal/ratelimiter/RateLimiter.java
+++ src/main/java/com/google/maps/internal/ratelimiter/RateLimiter.java
@@ -302,20 +302,6 @@ public abstract class RateLimiter {
   }
 
   /**
-   * Acquires permits from this {@link RateLimiter} if it can be acquired immediately without delay.
-   *
-   * <p>This method is equivalent to {@code tryAcquire(permits, 0, anyUnit)}.
-   *
-   * @param permits the number of permits to acquire
-   * @return {@code true} if the permits were acquired, {@code false} otherwise
-   * @throws IllegalArgumentException if the requested number of permits is negative or zero
-   * @since 14.0
-   */
-  public boolean tryAcquire(int permits) {
-    return tryAcquire(permits, 0, MICROSECONDS);
-  }
-
-  /**
    * Acquires a permit from this {@link RateLimiter} if it can be acquired immediately without
    * delay.
    *
@@ -402,7 +388,22 @@ public abstract class RateLimiter {
 
     protected abstract void sleepMicrosUninterruptibly(long micros);
 
-    public static SleepingStopwatch createFromSystemTimer() {
+    /**
+	   * Acquires permits from this {@link RateLimiter} if it can be acquired immediately without delay.
+	   *
+	   * <p>This method is equivalent to {@code tryAcquire(permits, 0, anyUnit)}.
+	   *
+	   * @param rateLimiter TODO
+	 * @param permits the number of permits to acquire
+	 * @return {@code true} if the permits were acquired, {@code false} otherwise
+	   * @throws IllegalArgumentException if the requested number of permits is negative or zero
+	   * @since 14.0
+	   */
+	  public boolean tryAcquire(RateLimiter rateLimiter, int permits) {
+	    return rateLimiter.tryAcquire(permits, 0, MICROSECONDS);
+	  }
+
+	public static SleepingStopwatch createFromSystemTimer() {
       return new SleepingStopwatch() {
         final Stopwatch stopwatch = Stopwatch.createStarted();
 
