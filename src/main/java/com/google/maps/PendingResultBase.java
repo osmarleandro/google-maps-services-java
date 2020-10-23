d0cab45cd59899c600c3e67df3043a9845ad55c0
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 20:49:16 -0300

    google-maps-services-java,com.google.maps,TextSearchRequest,region#QString,PullUpMethod,BUILD FAILED,false

--- src/main/java/com/google/maps/PendingResultBase.java
+++ src/main/java/com/google/maps/PendingResultBase.java
@@ -169,4 +169,17 @@ abstract class PendingResultBase<T, A extends PendingResultBase<T, A, R>, R exte
   public A custom(String parameter, String value) {
     return param(parameter, value);
   }
+
+/**
+   * Region used to influence search results. This parameter will only influence, not fully
+   * restrict, search results. If more relevant results exist outside of the specified region, they
+   * may be included. When this parameter is used, the country name is omitted from the resulting
+   * formatted_address for results in the specified region.
+   *
+   * @param region The ccTLD two-letter code of the region.
+   * @return Returns this {@code TextSearchRequest} for call chaining.
+   */
+public TextSearchRequest region(String region) {
+    return param("region", region);
+  }
 }
