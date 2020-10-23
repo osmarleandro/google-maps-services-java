91b348c2478032af6151e2fa8da4357253e9ca42
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 10:21:47 -0300

    google-maps-services-java,com.google.maps,GeoApiContextTest,createMockGoodResponse#,RenameMethod,OK,false

--- src/test/java/com/google/maps/GeoApiContextTest.java
+++ src/test/java/com/google/maps/GeoApiContextTest.java
@@ -110,7 +110,7 @@ public class GeoApiContextTest {
   public void testErrorResponseRetries() throws Exception {
     // Set up mock responses
     MockResponse errorResponse = createMockBadResponse();
-    MockResponse goodResponse = createMockGoodResponse();
+    MockResponse goodResponse = createMockGoodResponse_RENAMED();
 
     server.enqueue(errorResponse);
     server.enqueue(goodResponse);
@@ -132,7 +132,7 @@ public class GeoApiContextTest {
   @Test(expected = IOException.class)
   public void testSettingMaxRetries() throws Exception {
     MockResponse errorResponse = createMockBadResponse();
-    MockResponse goodResponse = createMockGoodResponse();
+    MockResponse goodResponse = createMockGoodResponse_RENAMED();
 
     // Set up the fake web server
     server.enqueue(errorResponse);
@@ -148,7 +148,7 @@ public class GeoApiContextTest {
     builder.build().get(new ApiConfig("/"), GeocodingApi.Response.class, "k", "v").await();
   }
 
-  private MockResponse createMockGoodResponse() {
+  private MockResponse createMockGoodResponse_RENAMED() {
     MockResponse response = new MockResponse();
     response.setResponseCode(200);
     response.setBody(
