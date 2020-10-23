d044ada2486820584e5ca10fd4005846daf0b5a8
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 20:41:03 -0300

    google-maps-services-java,com.google.maps,PendingResultBase,paramAddToList#QString;QString,PullUpMethod,BUILD FAILED,false

--- src/main/java/com/google/maps/internal/GaePendingResult.java
+++ src/main/java/com/google/maps/internal/GaePendingResult.java
@@ -261,4 +261,10 @@ public class GaePendingResult<T, R extends ApiResponse<T>> implements PendingRes
         && cumulativeSleepTime < errorTimeOut
         && (maxRetries == null || retryCounter < maxRetries);
   }
+
+@Override
+public A paramAddToList(String key, String val) {
+	// TODO Auto-generated method stub
+	return null;
+}
 }
--- src/main/java/com/google/maps/internal/OkHttpPendingResult.java
+++ src/main/java/com/google/maps/internal/OkHttpPendingResult.java
@@ -338,4 +338,10 @@ public class OkHttpPendingResult<T, R extends ApiResponse<T>>
         && cumulativeSleepTime < errorTimeOut
         && (maxRetries == null || retryCounter < maxRetries);
   }
+
+@Override
+public A paramAddToList(String key, String val) {
+	// TODO Auto-generated method stub
+	return null;
+}
 }
