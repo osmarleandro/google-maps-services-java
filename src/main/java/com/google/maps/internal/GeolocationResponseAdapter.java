08bab31382450de15fca863a8ff37c97b52fa018
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 09:26:26 -0300

    google-maps-services-java,com.google.maps.internal,GeolocationResponseAdapter,read#QJsonReader,RenameMethod,BUILD FAILED,false

--- src/main/java/com/google/maps/internal/GeolocationResponseAdapter.java
+++ src/main/java/com/google/maps/internal/GeolocationResponseAdapter.java
@@ -62,7 +62,7 @@ public class GeolocationResponseAdapter extends TypeAdapter<GeolocationApi.Respo
    * </pre>
    */
   @Override
-  public GeolocationApi.Response read(JsonReader reader) throws IOException {
+  public GeolocationApi.Response read_RENAMED(JsonReader reader) throws IOException {
 
     if (reader.peek() == JsonToken.NULL) {
       reader.nextNull();
@@ -78,7 +78,7 @@ public class GeolocationResponseAdapter extends TypeAdapter<GeolocationApi.Respo
       // "accuracy" keys or an error object containing an "error" key
       if (name.equals("location")) {
         // we already have a parser for the LatLng object so lets use that
-        response.location = latLngAdapter.read(reader);
+        response.location = latLngAdapter.read_RENAMED(reader);
       } else if (name.equals("accuracy")) {
         response.accuracy = reader.nextDouble();
       } else if (name.equals("error")) {
