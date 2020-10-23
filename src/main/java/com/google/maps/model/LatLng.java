8e80287215b1456e917c6855f32d84c4b1ae8b90
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 13:20:09 -0300

    google-maps-services-java,com.google.maps,PlacesApiTest,testTextSearchResponse#,MoveMethod,BUILD FAILED,false

--- src/main/java/com/google/maps/model/LatLng.java
+++ src/main/java/com/google/maps/model/LatLng.java
@@ -15,10 +15,21 @@
 
 package com.google.maps.model;
 
+import com.google.maps.LocalTestServerContext;
+import com.google.maps.PlacesApi;
+import com.google.maps.PlacesApiTest;
 import com.google.maps.internal.StringJoin.UrlValue;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+
 import java.io.Serializable;
+import java.net.URI;
 import java.util.Locale;
 import java.util.Objects;
+
+import org.junit.Test;
 
 /** A place on Earth, represented by a latitude/longitude pair. */
 public class LatLng implements UrlValue, Serializable {
@@ -67,5 +78,57 @@ public class LatLng implements UrlValue, Serializable {
   @Override
   public int hashCode() {
     return Objects.hash(lat, lng);
+  }
+
+@Test
+  public void testTextSearchResponse(PlacesApiTest placesApiTest) throws Exception {
+    try (LocalTestServerContext sc = new LocalTestServerContext(placesApiTest.textSearchResponseBody)) {
+      PlacesSearchResponse results = PlacesApi.textSearchQuery(sc.context, "Google Sydney").await();
+
+      assertNotNull(results);
+      assertNotNull(results.results);
+      assertEquals(1, results.results.length);
+      assertNotNull(results.toString());
+
+      PlacesSearchResult result = results.results[0];
+      assertNotNull(result.formattedAddress);
+      assertEquals("5, 48 Pirrama Rd, Pyrmont NSW 2009, Australia", result.formattedAddress);
+      assertNotNull(result.geometry);
+      assertNotNull(result.geometry.location);
+      assertEquals(-33.866611, result.geometry.location.lat, 0.0001);
+      assertEquals(151.195832, result.geometry.location.lng, 0.0001);
+      assertNotNull(result.icon);
+      assertEquals(
+          new URI("https://maps.gstatic.com/mapfiles/place_api/icons/generic_business-71.png"),
+          result.icon.toURI());
+      assertNotNull(result.name);
+      assertEquals("Google", result.name);
+      assertNotNull(result.openingHours);
+      assertFalse(result.openingHours.openNow);
+      assertNotNull(result.photos);
+
+      assertEquals(1, result.photos.length);
+      Photo photo = result.photos[0];
+      assertNotNull(photo);
+      assertEquals(2322, photo.height);
+      assertEquals(4128, photo.width);
+      assertNotNull(photo.htmlAttributions);
+      assertEquals(1, photo.htmlAttributions.length);
+      assertEquals(
+          "<a href=\"https://maps.google.com/maps/contrib/107252953636064841537\">William Stewart</a>",
+          photo.htmlAttributions[0]);
+      assertEquals(
+          "CmRdAAAAa43ZeiQvF4n-Yv5UnEGcIe0KjdTzzTH4g-g1GuKgWas0g8W7793eFDGxkrG4Z5i_Jua0Z-"
+              + "Ib88IuYe2iVAZ0W3Q7wUrp4A2mux4BjZmakLFkTkPj_OZ7ek3vSGnrzqExEhBqB3AIn82lmf38RnVSFH1CGhSWrvzN30A_"
+              + "ABGNScuiYEU70wau3w",
+          photo.photoReference);
+
+      assertNotNull(result.placeId);
+      assertEquals("ChIJN1t_tDeuEmsRUsoyG83frY4", result.placeId);
+      assertEquals(4.4, result.rating, 0.0001);
+      assertNotNull(result.types);
+      assertNotNull(result.types[0]);
+      assertEquals("establishment", result.types[0]);
+    }
   }
 }
