ce28c6fd31e148c3f6ed6c516989b332829c1ecc
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 09:14:07 -0300

    google-maps-services-java,com.google.maps,PendingResultBase,validateRequest#,RenameMethod,BUILD FAILED,true

--- src/main/java/com/google/maps/PendingResultBase.java
+++ src/main/java/com/google/maps/PendingResultBase.java
@@ -76,7 +76,7 @@ abstract class PendingResultBase<T, A extends PendingResultBase<T, A, R>, R exte
       throw new IllegalStateException(
           "'await', 'awaitIgnoreError' or 'setCallback' was already called.");
     }
-    validateRequest();
+    validateRequest_RENAMED();
     switch (config.requestVerb) {
       case "GET":
         return delegate = context.get(config, responseClass, params);
@@ -88,7 +88,7 @@ abstract class PendingResultBase<T, A extends PendingResultBase<T, A, R>, R exte
     }
   }
 
-  protected abstract void validateRequest();
+  protected abstract void validateRequest_RENAMED();
 
   private A getInstance() {
     @SuppressWarnings("unchecked")
