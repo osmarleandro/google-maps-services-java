578821511aba9754d243605edb743144a629b6ba
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 09:36:58 -0300

    google-maps-services-java,com.google.maps.metrics,OpenCensusMetrics,registerAllViews#,RenameMethod,OK,false

--- src/main/java/com/google/maps/metrics/OpenCensusMetrics.java
+++ src/main/java/com/google/maps/metrics/OpenCensusMetrics.java
@@ -108,7 +108,7 @@ public final class OpenCensusMetrics {
             fields);
   }
 
-  public static void registerAllViews() {
+  public static void registerAllViews_RENAMED() {
     registerAllViews(Stats.getViewManager());
   }
 
