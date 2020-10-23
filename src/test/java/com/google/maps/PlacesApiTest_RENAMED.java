620a1cae77b81ecf323fa4553dc87989fab9053d
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 11:08:01 -0300

    google-maps-services-java,com.google.maps,PlacesApiTest,NA,RenameType,OK,NA

--- src/test/java/com/google/maps/PlacesApiTest_RENAMED.java
+++ src/test/java/com/google/maps/PlacesApiTest_RENAMED.java
@@ -54,7 +54,7 @@ import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import org.junit.Test;
 
-public class PlacesApiTest {
+public class PlacesApiTest_RENAMED {
 
   private static final String GOOGLE_SYDNEY = "ChIJN1t_tDeuEmsRUsoyG83frY4";
   private static final String QUAY_PLACE_ID = "ChIJ02qnq0KuEmsRHUJF4zo1x4I";
@@ -82,7 +82,7 @@ public class PlacesApiTest {
   private final String placesApiKitaWard;
   private final String findPlaceFromTextMuseumOfContemporaryArt;
 
-  public PlacesApiTest() {
+  public PlacesApiTest_RENAMED() {
     autocompletePredictionStructuredFormatting =
         retrieveBody("AutocompletePredictionStructuredFormatting.json");
     placeDetailResponseBody = retrieveBody("PlaceDetailsResponse.json");
