5e8f3d27bfa016e44d73309a7306ff6598ad49ad
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 20:37:54 -0300

    google-maps-services-java,com.google.maps,StaticMapsRequest,size#QSize,PullUpMethod,BUILD FAILED,false

--- src/main/java/com/google/maps/PendingResultBase.java
+++ src/main/java/com/google/maps/PendingResultBase.java
@@ -19,6 +19,8 @@ import com.google.maps.errors.ApiException;
 import com.google.maps.internal.ApiConfig;
 import com.google.maps.internal.ApiResponse;
 import com.google.maps.internal.StringJoin.UrlValue;
+import com.google.maps.model.Size;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -168,5 +170,15 @@ abstract class PendingResultBase<T, A extends PendingResultBase<T, A, R>, R exte
    */
   public A custom(String parameter, String value) {
     return param(parameter, value);
+  }
+
+/**
+   * <code>size</code> defines the rectangular dimensions of the map image.
+   *
+   * @param size The Size of the static map.
+   * @return Returns this {@code StaticMapsRequest} for call chaining.
+   */
+public StaticMapsRequest size(Size size) {
+    return param("size", size);
   }
 }
