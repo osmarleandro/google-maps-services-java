9135c753463919300f9e67bc5acd0653a2ddb6af
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-23 09:35:17 -0300

    google-maps-services-java,com.google.maps.internal.ratelimiter,RateLimiter,canAcquire#J;J,RenameMethod,OK,false

--- src/main/java/com/google/maps/internal/ratelimiter/RateLimiter.java
+++ src/main/java/com/google/maps/internal/ratelimiter/RateLimiter.java
@@ -345,7 +345,7 @@ public abstract class RateLimiter {
     long microsToWait;
     synchronized (mutex()) {
       long nowMicros = stopwatch.readMicros();
-      if (!canAcquire(nowMicros, timeoutMicros)) {
+      if (!canAcquire_RENAMED(nowMicros, timeoutMicros)) {
         return false;
       } else {
         microsToWait = reserveAndGetWaitLength(permits, nowMicros);
@@ -355,7 +355,7 @@ public abstract class RateLimiter {
     return true;
   }
 
-  private boolean canAcquire(long nowMicros, long timeoutMicros) {
+  private boolean canAcquire_RENAMED(long nowMicros, long timeoutMicros) {
     return queryEarliestAvailable(nowMicros) - timeoutMicros <= nowMicros;
   }
 
