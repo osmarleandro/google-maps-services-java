07edd4d1eb011e79703a4cf6cdb61a87b1a2de7d
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-25 00:52:36 -0300

    google-maps-services-java,com.google.maps.errors,NotFoundException,NA,ExtractInterface,OK,NA

--- src/main/java/com/google/maps/errors/NotFoundException.java
+++ src/main/java/com/google/maps/errors/NotFoundException.java
@@ -19,7 +19,7 @@ package com.google.maps.errors;
  * Indicates at least one of the locations specified in the request's origin, destination, or
  * waypoints could not be geocoded.
  */
-public class NotFoundException extends ApiException {
+public class NotFoundException extends ApiException implements INotFoundException {
 
   private static final long serialVersionUID = -5447625132975504651L;
 
