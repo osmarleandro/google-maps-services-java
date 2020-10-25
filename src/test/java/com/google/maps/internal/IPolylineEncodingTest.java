f720cfabe30aafeae79d5f6f96e7bf392ce0022f
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-25 02:19:45 -0300

    google-maps-services-java,com.google.maps.internal,PolylineEncodingTest,NA,ExtractInterface,OK,NA

---
+++ src/test/java/com/google/maps/internal/IPolylineEncodingTest.java
@@ -0,0 +1,11 @@
+package com.google.maps.internal;
+
+import org.junit.Test;
+
+public interface IPolylineEncodingTest {
+
+	void testPolylineEncodingRoundTrip() throws Exception;
+
+	void testDecode() throws Exception;
+
+}
