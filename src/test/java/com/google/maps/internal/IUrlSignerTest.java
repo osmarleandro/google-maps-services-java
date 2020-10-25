bd7a2fb10e7c0c3fc1feff283edb3914dc635eb2
Osmar Leandro <osmarleandrods@gmail.com>
2020-10-25 02:20:36 -0300

    google-maps-services-java,com.google.maps.internal,UrlSignerTest,NA,ExtractInterface,OK,NA

---
+++ src/test/java/com/google/maps/internal/IUrlSignerTest.java
@@ -0,0 +1,11 @@
+package com.google.maps.internal;
+
+import org.junit.Test;
+
+public interface IUrlSignerTest {
+
+	void testUrlSigner() throws Exception;
+
+	void testMustSupportParallelSignatures() throws Exception;
+
+}
