4d3a2e716a79fea352018c394773e2db431d699c
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-02 05:18:45 -0300

    google-maps-services-java,com.google.maps.model,LatLng,equals#QObject,InlineMethod,OK,false

--- src/main/java/com/google/maps/model/LatLng.java
+++ src/main/java/com/google/maps/model/LatLng.java
@@ -57,14 +57,6 @@ public class LatLng implements UrlValue, Serializable {
   }
 
   @Override
-  public boolean equals(Object o) {
-    if (this == o) return true;
-    if (o == null || getClass() != o.getClass()) return false;
-    LatLng latLng = (LatLng) o;
-    return Double.compare(latLng.lat, lat) == 0 && Double.compare(latLng.lng, lng) == 0;
-  }
-
-  @Override
   public int hashCode() {
     return Objects.hash(lat, lng);
   }
