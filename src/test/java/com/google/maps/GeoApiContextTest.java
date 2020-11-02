d42eabcb5c745d8e1f64246fd80b6cba1ac7c02b
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-02 06:29:02 -0300

    google-maps-services-java,com.google.maps,GeoApiContextTest,testExperienceIdNotInHeader#,InlineMethod,OK,false

--- src/test/java/com/google/maps/GeoApiContextTest.java
+++ src/test/java/com/google/maps/GeoApiContextTest.java
@@ -341,13 +341,6 @@ public class GeoApiContextTest {
   }
 
   @Test
-  public void testExperienceIdNotInHeader() throws Exception {
-    final RecordedRequest request = makeMockRequest();
-    final String value = request.getHeader(HttpHeaders.X_GOOG_MAPS_EXPERIENCE_ID);
-    assertNull(value);
-  }
-
-  @Test
   public void testExperienceIdSample() {
     // [START maps_experience_id]
     final String experienceId = UUID.randomUUID().toString();
