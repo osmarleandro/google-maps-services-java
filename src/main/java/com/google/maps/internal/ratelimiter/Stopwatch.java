2e1d887e30ce741d541b3d2627a63d41385a8128
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-02 02:39:03 -0300

    google-maps-services-java,com.google.maps.internal.ratelimiter,Stopwatch,reset#,InlineMethod,OK,false

--- src/main/java/com/google/maps/internal/ratelimiter/Stopwatch.java
+++ src/main/java/com/google/maps/internal/ratelimiter/Stopwatch.java
@@ -159,17 +159,6 @@ public final class Stopwatch {
     return this;
   }
 
-  /**
-   * Sets the elapsed time for this stopwatch to zero, and places it in a stopped state.
-   *
-   * @return this {@code Stopwatch} instance
-   */
-  public Stopwatch reset() {
-    elapsedNanos = 0;
-    isRunning = false;
-    return this;
-  }
-
   private long elapsedNanos() {
     return isRunning ? ticker.read() - startTick + elapsedNanos : elapsedNanos;
   }
