808009b8d38b412561825931f391a0f906ab9ab2
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-02 04:41:20 -0300

    google-maps-services-java,com.google.maps.model,TransitLine,toString#,InlineMethod,OK,false

--- src/main/java/com/google/maps/model/TransitLine.java
+++ src/main/java/com/google/maps/model/TransitLine.java
@@ -60,9 +60,4 @@ public class TransitLine implements Serializable {
 
   /** The type of vehicle used on this transit line. */
   public Vehicle vehicle;
-
-  @Override
-  public String toString() {
-    return String.format("%s \"%s\"", shortName, name);
-  }
 }
