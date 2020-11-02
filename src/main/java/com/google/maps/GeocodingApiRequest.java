9a2bcf523637e26c196e60e3c5716f17d1973b3c
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-02 01:13:35 -0300

    google-maps-services-java,com.google.maps,GeocodingApiRequest,validateRequest#,InlineMethod,BUILD FAILED,false

--- src/main/java/com/google/maps/GeocodingApiRequest.java
+++ src/main/java/com/google/maps/GeocodingApiRequest.java
@@ -34,26 +34,6 @@ public class GeocodingApiRequest
     super(context, API_CONFIG, GeocodingApi.Response.class);
   }
 
-  @Override
-  protected void validateRequest() {
-    // Must not have both address and latlng.
-    if (params().containsKey("latlng")
-        && params().containsKey("address")
-        && params().containsKey("place_id")) {
-      throw new IllegalArgumentException(
-          "Request must contain only one of 'address', 'latlng' or 'place_id'.");
-    }
-
-    // Must contain at least one of place_id, address, latlng, and components;
-    if (!params().containsKey("latlng")
-        && !params().containsKey("address")
-        && !params().containsKey("components")
-        && !params().containsKey("place_id")) {
-      throw new IllegalArgumentException(
-          "Request must contain at least one of 'address', 'latlng', 'place_id' and 'components'.");
-    }
-  }
-
   /**
    * Creates a forward geocode for {@code address}.
    *
