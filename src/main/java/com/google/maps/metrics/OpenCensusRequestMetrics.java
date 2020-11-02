c456bcf99b664abe40b51d25a22c5e36c0fe1c8b
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-02 02:55:52 -0300

    google-maps-services-java,com.google.maps.metrics,OpenCensusRequestMetrics,endRequest#QException;I;J,InlineMethod,BUILD FAILED,false

--- src/main/java/com/google/maps/metrics/OpenCensusRequestMetrics.java
+++ src/main/java/com/google/maps/metrics/OpenCensusRequestMetrics.java
@@ -36,31 +36,6 @@ final class OpenCensusRequestMetrics implements RequestMetrics {
     this.networkTime += milliTime() - this.networkStart;
   }
 
-  @Override
-  public void endRequest(Exception exception, int httpStatusCode, long retryCount) {
-    // multiple endRequest are ignored
-    if (this.finished) {
-      return;
-    }
-    this.finished = true;
-    long requestTime = milliTime() - this.requestStart;
-
-    TagContext tagContext =
-        tagger
-            .currentBuilder()
-            .putLocal(OpenCensusMetrics.Tags.REQUEST_NAME, TagValue.create(requestName))
-            .putLocal(
-                OpenCensusMetrics.Tags.HTTP_CODE, TagValue.create(Integer.toString(httpStatusCode)))
-            .putLocal(OpenCensusMetrics.Tags.API_STATUS, TagValue.create(exceptionName(exception)))
-            .build();
-    statsRecorder
-        .newMeasureMap()
-        .put(OpenCensusMetrics.Measures.LATENCY, requestTime)
-        .put(OpenCensusMetrics.Measures.NETWORK_LATENCY, this.networkTime)
-        .put(OpenCensusMetrics.Measures.RETRY_COUNT, retryCount)
-        .record(tagContext);
-  }
-
   private String exceptionName(Exception exception) {
     if (exception == null) {
       return "";
