211ebfe38efdb24357a2da1e7067c482d925abcd
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-02 02:03:39 -0300

    google-maps-services-java,com.google.maps.internal,DistanceAdapter,read#QJsonReader,InlineMethod,BUILD FAILED,false

--- src/main/java/com/google/maps/internal/DistanceAdapter.java
+++ src/main/java/com/google/maps/internal/DistanceAdapter.java
@@ -31,40 +31,6 @@ import java.io.IOException;
  */
 public class DistanceAdapter extends TypeAdapter<Distance> {
 
-  /**
-   * Read a distance object from a Directions API result and convert it to a {@link Distance}.
-   *
-   * <p>We are expecting to receive something akin to the following:
-   *
-   * <pre>
-   * {
-   *   "value": 207, "text": "0.1 mi"
-   * }
-   * </pre>
-   */
-  @Override
-  public Distance read(JsonReader reader) throws IOException {
-    if (reader.peek() == JsonToken.NULL) {
-      reader.nextNull();
-      return null;
-    }
-
-    Distance distance = new Distance();
-
-    reader.beginObject();
-    while (reader.hasNext()) {
-      String name = reader.nextName();
-      if (name.equals("text")) {
-        distance.humanReadable = reader.nextString();
-      } else if (name.equals("value")) {
-        distance.inMeters = reader.nextLong();
-      }
-    }
-    reader.endObject();
-
-    return distance;
-  }
-
   /** This method is not implemented. */
   @Override
   public void write(JsonWriter writer, Distance value) throws IOException {
