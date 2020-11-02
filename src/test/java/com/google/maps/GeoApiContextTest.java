bb7816b423f0cd56fa78109868a83d8faec0cb44
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-02 06:31:48 -0300

    google-maps-services-java,com.google.maps,GeoApiContextTest,testShutdown#,InlineMethod,OK,false

--- src/test/java/com/google/maps/GeoApiContextTest.java
+++ src/test/java/com/google/maps/GeoApiContextTest.java
@@ -394,20 +394,4 @@ public class GeoApiContextTest {
     server.shutdown();
     return server.takeRequest();
   }
-
-  @Test
-  public void testShutdown() throws InterruptedException {
-    GeoApiContext context = builder.build();
-    final Thread delayThread = findLastThreadByName("RateLimitExecutorDelayThread");
-    assertNotNull(
-        "Delay thread should be created in constructor of RateLimitExecutorService", delayThread);
-    assertTrue(
-        "Delay thread should start in constructor of RateLimitExecutorService",
-        delayThread.isAlive());
-    // this is needed to make sure that delay thread has reached queue.take()
-    delayThread.join(10);
-    context.shutdown();
-    delayThread.join(10);
-    assertFalse(delayThread.isAlive());
-  }
 }
