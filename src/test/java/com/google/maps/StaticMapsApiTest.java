432559215a450638c0c89bdc6c8a4a57a03bee15
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-02 01:14:12 -0300

    google-maps-services-java,com.google.maps,StaticMapsRequest,center#QLatLng,InlineMethod,OK,false

--- src/test/java/com/google/maps/StaticMapsApiTest.java
+++ src/test/java/com/google/maps/StaticMapsApiTest.java
@@ -70,7 +70,7 @@ public class StaticMapsApiTest {
     try (LocalTestServerContext sc = new LocalTestServerContext(IMAGE)) {
 
       StaticMapsRequest req = StaticMapsApi.newRequest(sc.context, new Size(WIDTH, HEIGHT));
-      req.center(SYDNEY);
+      req.param("center", SYDNEY);
       req.zoom(16);
       req.await();
 
