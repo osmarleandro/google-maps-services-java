e161fb06078132d5edde1a6907550507092cee86
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-09 17:52:36 -0300

    google-maps-services-java,com.google.maps,GeolocationApiTest,testNoPayloadGeolocation0#,ExtractMethod,OK,false

--- src/test/java/com/google/maps/GeolocationApiTest.java
+++ src/test/java/com/google/maps/GeolocationApiTest.java
@@ -19,6 +19,9 @@ import static com.google.maps.TestUtils.retrieveBody;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import java.io.IOException;
+
+import com.google.maps.errors.ApiException;
 import com.google.maps.errors.InvalidRequestException;
 import com.google.maps.errors.NotFoundException;
 import com.google.maps.model.CellTower;
@@ -373,15 +376,20 @@ public class GeolocationApiTest {
   @Test
   public void testNoPayloadGeolocation0() throws Exception {
     try (LocalTestServerContext sc = new LocalTestServerContext(geolocationBasic)) {
-      GeolocationPayload payload =
-          new GeolocationPayload.GeolocationPayloadBuilder().createGeolocationPayload();
-
-      GeolocationResult result = GeolocationApi.geolocate(sc.context, payload).await();
+      GeolocationResult result = extracted(sc);
       assertNotNull(result);
       assertNotNull(result.toString());
       assertNotNull(result.location);
     }
   }
+
+private GeolocationResult extracted(LocalTestServerContext sc) throws ApiException, InterruptedException, IOException {
+	GeolocationPayload payload =
+          new GeolocationPayload.GeolocationPayloadBuilder().createGeolocationPayload();
+
+      GeolocationResult result = GeolocationApi.geolocate(sc.context, payload).await();
+	return result;
+}
 
   @Test
   public void testNoPayloadGeolocation1() throws Exception {
