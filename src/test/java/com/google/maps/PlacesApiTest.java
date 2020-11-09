eb88da5329ee2c7f6e0eeca1f3f792ed23b70886
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-09 17:46:14 -0300

    google-maps-services-java,com.google.maps,PlacesApiTest,testTextSearch#,ExtractMethod,OK,false

--- src/test/java/com/google/maps/PlacesApiTest.java
+++ src/test/java/com/google/maps/PlacesApiTest.java
@@ -28,6 +28,7 @@ import com.google.maps.FindPlaceFromTextRequest.LocationBiasPoint;
 import com.google.maps.FindPlaceFromTextRequest.LocationBiasRectangular;
 import com.google.maps.PlaceAutocompleteRequest.SessionToken;
 import com.google.maps.PlaceDetailsRequest.FieldMask;
+import com.google.maps.errors.ApiException;
 import com.google.maps.model.AddressComponentType;
 import com.google.maps.model.AddressType;
 import com.google.maps.model.AutocompletePrediction;
@@ -47,7 +48,10 @@ import com.google.maps.model.PlacesSearchResponse;
 import com.google.maps.model.PlacesSearchResult;
 import com.google.maps.model.PriceLevel;
 import com.google.maps.model.RankBy;
+
+import java.io.IOException;
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.LocalTime;
 import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
@@ -712,10 +716,7 @@ public class PlacesApiTest {
   @Test
   public void testTextSearch() throws Exception {
     try (LocalTestServerContext sc = new LocalTestServerContext(placesApiTextSearch)) {
-      PlacesSearchResponse response =
-          PlacesApi.textSearchQuery(sc.context, "Google Sydney").await();
-
-      sc.assertParamValue("Google Sydney", "query");
+      PlacesSearchResponse response = extracted(sc);
 
       assertNotNull(response.toString());
       assertEquals(1, response.results.length);
@@ -725,6 +726,15 @@ public class PlacesApiTest {
       assertEquals("OPERATIONAL", result.businessStatus);
     }
   }
+
+private PlacesSearchResponse extracted(LocalTestServerContext sc)
+		throws ApiException, InterruptedException, IOException, URISyntaxException {
+	PlacesSearchResponse response =
+          PlacesApi.textSearchQuery(sc.context, "Google Sydney").await();
+
+      sc.assertParamValue("Google Sydney", "query");
+	return response;
+}
 
   @Test
   public void testPhoto() throws Exception {
