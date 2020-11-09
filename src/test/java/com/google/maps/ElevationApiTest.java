2d22381928dd2a164a251d3056cbdfaa1e5ede3f
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-09 18:00:10 -0300

    google-maps-services-java,com.google.maps,ElevationApiTest,testGetPoints#,ExtractMethod,OK,false

--- src/test/java/com/google/maps/ElevationApiTest.java
+++ src/test/java/com/google/maps/ElevationApiTest.java
@@ -19,12 +19,15 @@ import static com.google.maps.TestUtils.retrieveBody;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
+import com.google.maps.errors.ApiException;
 import com.google.maps.errors.InvalidRequestException;
 import com.google.maps.errors.RequestDeniedException;
 import com.google.maps.model.ElevationResult;
 import com.google.maps.model.EncodedPolyline;
 import com.google.maps.model.LatLng;
 import com.google.maps.model.LatLngAssert;
+
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import org.junit.Test;
@@ -146,9 +149,7 @@ public class ElevationApiTest {
                 + "   ],\n"
                 + "   \"status\" : \"OK\"\n"
                 + "}\n")) {
-      ElevationResult[] results = ElevationApi.getByPoints(sc.context, SYDNEY, MELBOURNE).await();
-
-      assertNotNull(results);
+      ElevationResult[] results = extracted(sc);
       assertEquals(2, results.length);
       assertEquals(SYDNEY_ELEVATION, results[0].elevation, EPSILON);
       assertEquals(MELBOURNE_ELEVATION, results[1].elevation, EPSILON);
@@ -156,6 +157,13 @@ public class ElevationApiTest {
       sc.assertParamValue("enc:xvumEur{y[jyaWdnbe@", "locations");
     }
   }
+
+private ElevationResult[] extracted(LocalTestServerContext sc) throws ApiException, InterruptedException, IOException {
+	ElevationResult[] results = ElevationApi.getByPoints(sc.context, SYDNEY, MELBOURNE).await();
+
+      assertNotNull(results);
+	return results;
+}
 
   @Test
   public void testGetPath() throws Exception {
