34403fe690fb20fcb9ad350b8d4b02330c703574
Osmar Leandro <osmarleandrods@gmail.com>
2020-11-09 17:47:29 -0300

    google-maps-services-java,com.google.maps,PlacesApiTest,testPlaceDetailsWithBusinessStatus#,ExtractMethod,OK,false

--- src/test/java/com/google/maps/PlacesApiTest.java
+++ src/test/java/com/google/maps/PlacesApiTest.java
@@ -47,6 +47,8 @@ import com.google.maps.model.PlacesSearchResponse;
 import com.google.maps.model.PlacesSearchResult;
 import com.google.maps.model.PriceLevel;
 import com.google.maps.model.RankBy;
+
+import java.io.IOException;
 import java.net.URI;
 import java.time.LocalTime;
 import java.time.ZoneOffset;
@@ -1034,17 +1036,21 @@ public class PlacesApiTest {
 
   @Test
   public void testPlaceDetailsWithBusinessStatus() throws Exception {
-    final String jsonString = retrieveBody("PlaceDetailsResponseWithBusinessStatus.json");
-    final LocalTestServerContext server = new LocalTestServerContext(jsonString);
+    final LocalTestServerContext server = extracted();
     final PlaceDetails placeDetails = PlacesApi.placeDetails(server.context, "testPlaceId").await();
     assertNotNull(placeDetails);
     assertEquals("OPERATIONAL", placeDetails.businessStatus);
   }
 
+private LocalTestServerContext extracted() throws IOException {
+	final String jsonString = retrieveBody("PlaceDetailsResponseWithBusinessStatus.json");
+    final LocalTestServerContext server = new LocalTestServerContext(jsonString);
+	return server;
+}
+
   @Test
   public void testPlaceDetailsRequestHasFieldMask() throws Exception {
-    final String jsonString = retrieveBody("PlaceDetailsResponseWithBusinessStatus.json");
-    final LocalTestServerContext server = new LocalTestServerContext(jsonString);
+    final LocalTestServerContext server = extracted();
 
     PlacesApi.placeDetails(server.context, "testPlaceId").fields(FieldMask.BUSINESS_STATUS).await();
 
